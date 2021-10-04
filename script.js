7c25d41d048a5f144bab6050e45326587d18e89b
Dorota Boinska <dboinska@gmail.com>
2021-10-04 23:23:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const svgs = [...document.querySelectorAll("svg")];
+// console.log(svgs);
+// svgs.forEach((svg) => {
+//   console.log(svg);
+//   svg.style = "fill: red";
+//   console.log("dfdssd");
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const svgs = [...document.querySelectorAll("svg")];
+//   console.log(svgs);
+//   svgs.forEach((svg) => {
+//     console.log(svg);
+//     svg.style = "fill: red";
+//     console.log("dfdssd");
+//   });
+// });
